0e255b7f098cf9fe9d05640b05f4d582db3c197d
knutekje <knutekjersem@gmail.com>
2024-08-22 12:22:05 +0200

    adding https

--- obj/Debug/net8.0/Reported.AssemblyInfo.cs
+++ obj/Debug/net8.0/Reported.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Reported")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+772473fffa767391c59ae88c4d6b2e578f440405")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b7f7af8bbdb862ee452f664965fe7feef254555")]
 [assembly: System.Reflection.AssemblyProductAttribute("Reported")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Reported")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
